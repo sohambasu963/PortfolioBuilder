a6ab02a7621309fad2a4b72bf6f80cbf83517c64
Soham Basu <sohambasu963@gmail.com>
2024-01-15 15:44:30 -0500

    refactored backend and vercel config

--- server/api/index.py
+++ server/api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from main import calculate_weights
+from functions import calculate_weights
 
 app = Flask(__name__)
 CORS(app)
