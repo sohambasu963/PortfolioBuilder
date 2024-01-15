5a0cc5cb9b588bad13a341bda944f4b1c07ad9b0
Soham Basu <sohambasu963@gmail.com>
2024-01-15 15:50:29 -0500

    updated vercel config

--- server/api/index.py
+++ server/api/index.py
@@ -15,5 +15,5 @@ def get_weights():
     data = calculate_weights(data)
     return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
